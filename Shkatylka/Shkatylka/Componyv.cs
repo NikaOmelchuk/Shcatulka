c36d51a218ffc8f0448a901512d268056c68dd57
NikaOmelchuk <120196531+NikaOmelchuk@users.noreply.github.com>
2023-04-11 05:27:55 +0300

    Add files via upload

---
+++ Shkatylka/Shkatylka/Componyv.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shkatylka
+{
+    public abstract class Comp
+    {
+        public int x, y, h, w;
+        public Bitmap pic;
+        public abstract void move(int n);
+    }
+
+    public class dragoz1 : Comp
+    {
+        public dragoz1(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            h = 50; w = 50;
+            pic = new Bitmap(@"C:\Users\Саша\Desktop\Визуалка\лаби арітектура\Келементи\д1.png");
+        }
+
+        public override void move(int n)
+        {
+            switch(n)
+            {
+                case 1:
+                    x += 5;
+                    break;
+                case 2:
+                    x -= 5;
+                    break;
+                case 3:
+                    y += 5;
+                    break;
+                case 4:
+                    y -= 5;
+                    break;
+            }
+        }
+    }
+
+
+    public class dragoz2 : Comp
+    {
+        public dragoz2(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            h = 50; w = 50;
+            pic = new Bitmap(@"C:\Users\Саша\Desktop\Визуалка\лаби арітектура\Келементи\д3.png");
+        }
+
+        public override void move(int n)
+        {
+            switch (n)
+            {
+                case 1:
+                    x += 5;
+                    break;
+                case 2:
+                    x -= 5;
+                    break;
+                case 3:
+                    y += 5;
+                    break;
+                case 4:
+                    y -= 5;
+                    break;
+            }
+        }
+    }
+
+    public  class Componov: Comp
+    {
+        public List<Comp> masc = new List<Comp>();
+
+        public Componov(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            h = 500; w = 400;
+            pic = new Bitmap(@"C:\Users\Саша\Desktop\Визуалка\лаби арітектура\Келементи\шкатулка.png");
+        }
+
+        public void add(Comp c)
+        {
+            masc.Add(c);
+        }
+
+        public void remove(Comp c)
+        {
+            masc.Remove(c);
+        }
+
+        public override void move(int n)
+        {
+
+            switch (n)
+            {
+                case 1:
+                    x += 5;
+                    for (int i = 0; i < masc.Count; i++)
+                        masc[i].move(1);
+                    break;
+                case 2:
+                    x -= 5;
+                    for (int i = 0; i < masc.Count; i++)
+                        masc[i].move(2);
+                    break;
+                case 3:
+                    y += 5;
+                    for (int i = 0; i < masc.Count; i++)
+                        masc[i].move(3);
+                    break;
+                case 4:
+                    y -= 5;
+                    for (int i = 0; i < masc.Count; i++)
+                        masc[i].move(4);
+                    break;
+            }
+        }
+    }
+}
---
+++ Shkatylka/Shkatylka/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shkatylka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+
+        Graphics g;
+        Componov cp;
+        Comp cur;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button6.Enabled = true;
+            button7.Enabled = true;
+
+            cp = new Componov(150,10);
+            g.DrawImage(cp.pic, new Rectangle(cp.x, cp.y, cp.h, cp.w));
+
+            button5.Enabled = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            cp.remove(cur);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g.FillRectangle(new SolidBrush(Color.LightGray), new Rectangle(cp.x, cp.y, cp.h, cp.w));
+            cp.move(4);
+            g.DrawImage(cp.pic, new Rectangle(cp.x, cp.y, cp.h, cp.w));
+            for (int i = 0; i < cp.masc.Count; i++)
+            {
+                g.DrawImage(cp.masc[i].pic, new Rectangle(cp.masc[i].x, cp.masc[i].y, cp.masc[i].h, cp.masc[i].w));
+            }
+        }
+        Random r = new Random();
+        private void button6_Click(object sender, EventArgs e)
+        {
+            dragoz1 d1 = new dragoz1(r.Next(cp.x + 100, cp.h-10), r.Next(cp.y + 100, cp.w - 50));
+            cp.add(d1);
+
+            g.DrawImage(d1.pic, new Rectangle(d1.x, d1.y, d1.h, d1.w));
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            dragoz2 d2 = new dragoz2(r.Next(cp.x + 100, cp.h - 10), r.Next(cp.y + 100, cp.w - 50));
+            cp.add(d2);
+
+            g.DrawImage(d2.pic, new Rectangle(d2.x, d2.y, d2.h, d2.w));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            g.FillRectangle(new SolidBrush(Color.LightGray), new Rectangle(cp.x, cp.y, cp.h, cp.w));
+            cp.move(2);
+            g.DrawImage(cp.pic, new Rectangle(cp.x, cp.y, cp.h, cp.w));
+            for (int i = 0; i < cp.masc.Count; i++)
+            {
+                g.DrawImage(cp.masc[i].pic, new Rectangle(cp.masc[i].x, cp.masc[i].y, cp.masc[i].h, cp.masc[i].w));
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            g.FillRectangle(new SolidBrush(Color.LightGray), new Rectangle(cp.x, cp.y, cp.h, cp.w));
+            cp.move(3);
+            g.DrawImage(cp.pic, new Rectangle(cp.x, cp.y, cp.h, cp.w));
+            for (int i = 0; i < cp.masc.Count; i++)
+            {
+                g.DrawImage(cp.masc[i].pic, new Rectangle(cp.masc[i].x, cp.masc[i].y, cp.masc[i].h, cp.masc[i].w));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            g.FillRectangle(new SolidBrush(Color.LightGray), new Rectangle(cp.x, cp.y, cp.h, cp.w));
+            cp.move(1);
+            g.DrawImage(cp.pic, new Rectangle(cp.x, cp.y, cp.h, cp.w));
+            for (int i = 0; i < cp.masc.Count; i++)
+            {
+                g.DrawImage(cp.masc[i].pic, new Rectangle(cp.masc[i].x, cp.masc[i].y, cp.masc[i].h, cp.masc[i].w));
+            }
+        }
+    }
+}
